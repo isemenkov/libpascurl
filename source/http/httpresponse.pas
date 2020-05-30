e434a17af2aed3f9f9607bbbec92c92b3140e5d1
iXakeR <ivan@semenkov.pro>
2020-05-30 13:30:01 +0300

    Update THTTPResponse.TRedirect class

--- source/http/httpresponse.pas
+++ source/http/httpresponse.pas
@@ -54,10 +54,8 @@ type
         { Return TRUE if has errors }
         function HasErrors : Boolean;
           {$IFNDEF DEBUG}inline;{$ENDIF}
-        //function ErrorMessage : String;
-        //  {$IFNDEF DEBUG}inline;{$ENDIF}
-        //function OsErrno : Longint;
-        //  {$IFNDEF DEBUG}inline;{$ENDIF}
+
+        { Return error stack }
         function Errors : TErrorStack;
           {$IFNDEF DEBUG}inline;{$ENDIF}
       private
@@ -68,18 +66,29 @@ type
         FErrorBuffer : array [0 .. CURL_ERROR_SIZE] of char;
       end;
 
+      { HTTP(S) session redirected options }
       TRedirect = class
       public
+        { Return TRUE if request is redirected }
         function IsRedirected : Boolean;
+
+        { Return redirect count times }
         function Count : Longint;
+
+        { Return redirected URL }
         function Url : String;
+
+        { Return the time for all redirection steps }
+        function TotalTime : TTimeInterval;
       private
-        constructor Create;
+        constructor Create(ACurl : CURL; AErrors : PErrorStack);
       private
         FCurl : CURL;
+        FErrors : PErrorStack;
       end;
   private
     FError : TError;
+    FRedirect : TRedirect;
   end;
 
 implementation
@@ -108,24 +117,52 @@ end;
 
 { THTTPResponse.TRedirect }
 
-constructor THTTPResponse.TRedirect.Create;
+constructor THTTPResponse.TRedirect.Create (ACurl : CURL; AErrors : PErrorStack);
 begin
+  FCurl := ACurl;
+  FErrors := AErorrs;
 
+  FollowRedirect := True;
 end;
 
 function THTTPResponse.TRedirect.IsRedirected : Boolean;
 begin
-
+  Result := Count > 0;
 end;
 
 function THTTPResponse.TRedirect.Count : Longint;
 begin
-
+  Result := 0;
+  FErrors^.Push(curl_easy_getinfo(FCurl, CURLINFO_REDIRECT_COUNT, @Result));
 end;
 
 function THTTPResponse.TRedirect.Url : String;
+var
+  url : PChar;
 begin
+  New(url);
+  url := '';
+  FErrors^.Push(curl_easy_getinfo(FCurl, CURLINFO_REDIRECT_URL, @url));
+  Result := url;
+end;
 
+function THTTPResponse.TRedirect.TotalTime : TTimeInterval;
+var
+  time : Longword = 0;
+  dtime : Double = 0;
+  CurlResult : CURLcode;
+begin
+  CurlResult := curl_easy_getinfo(FCurl, CURLINFO_REDIRECT_TIME_T, @time);
+  Result := TTimeInterval.Create;
+  Result.Milliseconds := time;
+
+  if CurlResult <> CURLE_OK then
+  begin
+    CurlResult := curl_easy_getinfo(FCurl, CURLINFO_REDIRECT_TIME, @dtime);
+    Result.Milliseconds := ceil(dtime);
+  end;
+
+  FErrors^.Push(CurlResult);
 end;
 
 end.
