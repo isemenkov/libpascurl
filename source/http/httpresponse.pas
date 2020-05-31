78381831cbf736be02c665a9dd14ccf5bd4cffe1
iXakeR <ivan@semenkov.pro>
2020-05-31 10:07:22 +0300

    Add TResponse.TTimeout class

--- source/http/httpresponse.pas
+++ source/http/httpresponse.pas
@@ -71,17 +71,64 @@ type
       public
         { Return TRUE if request is redirected }
         function IsRedirected : Boolean;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
 
         { Return redirect count times }
         function Count : Longint;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
 
         { Return redirected URL }
         function Url : String;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
 
         { Return the time for all redirection steps }
         function TotalTime : TTimeInterval;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
       private
         constructor Create(ACurl : CURL; AErrors : PErrorStack);
+      private
+        FCurl : CURL;
+        FErrors : PErrorStack;
+      end;
+
+      { HTTP(S) session headers }
+      THeader = class
+      private
+        constructor Create (AHeaders : TStringList; AErrors : PErrorStack);
+        destructor Destroy; override;
+      private
+        FHeaders : TStringList;
+        FErrors : PErrorStack;
+      end;
+
+      { HTTP(S) session timeouts }
+      TTimeout = class
+      public
+        { Get transfer total time }
+        function Total : TTimeInterval;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
+
+        { Get the name lookup time }
+        function NameLookup : TTimeInterval;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
+
+        { Get the time until connect }
+        function Connect : TTimeInterval;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
+
+        { Get the time until the SSL/SSH handshake is completed }
+        function AppConnect : TTimeInterval;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
+
+        { Get the time until the file transfer start }
+        function PreTransfer : TTimeInterval;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
+
+        { Get time until the first byte is received }
+        function StartTransfer : TTimeInterval;
+          {$IFNDEF DEBUG}inline;{$ENDIF}
+      private
+        constructor Create (ACurl : CURL; AErrors : PErrorStack);
       private
         FCurl : CURL;
         FErrors : PErrorStack;
@@ -89,6 +136,7 @@ type
   private
     FError : TError;
     FRedirect : TRedirect;
+    FTimeout : TTimeout;
   end;
 
 implementation
@@ -117,7 +165,8 @@ end;
 
 { THTTPResponse.TRedirect }
 
-constructor THTTPResponse.TRedirect.Create (ACurl : CURL; AErrors : PErrorStack);
+constructor THTTPResponse.TRedirect.Create (ACurl : CURL;
+  AErrors : PErrorStack);
 begin
   FCurl := ACurl;
   FErrors := AErorrs;
@@ -159,6 +208,145 @@ begin
   if CurlResult <> CURLE_OK then
   begin
     CurlResult := curl_easy_getinfo(FCurl, CURLINFO_REDIRECT_TIME, @dtime);
+    Result.Milliseconds := ceil(dtime);
+  end;
+
+  FErrors^.Push(CurlResult);
+end;
+
+{ THTTPResponse.THeader }
+
+constructor THTTPResponse.THeader.Create(AHeaders : TStringList;
+  AErrors : PErrorStack);
+begin
+  FHeaders := AHeaders;
+  FErrors := AErrors;
+
+
+end;
+
+destructor THTTPResponse.THeader.Destroy;
+begin
+  FreeAndNil(AHeader);
+  inherited Destroy;
+end;
+
+{ THTTPResponse.TTimeout }
+
+constructor THTTPResponse.TTimeout.Create(ACurl : CURL; AErrors : PErrorStack);
+begin
+  FCurl := ACurl;
+  FErrors := AErrors;
+end;
+
+function THTTPResponse.TTimeout.Total : TTimeInterval;
+var
+  time : Longword = 0;
+  dtime : Double = 0;
+  CurlResult : CURLcode;
+begin
+  CurlResult := curl_easy_getinfo(FCurl, CURLINFO_TOTAL_TIME_T, @time);
+  Result := TTimeInterval.Create;
+  Result.Milliseconds := time;
+
+  if CurlResult <> CURLE_OK then
+  begin
+    CurlResult := curl_easy_getinfo(FCurl, CURLINFO_TOTAL_TIME, @dtime);
+    Result.Milliseconds := ceil(dtime);
+  end;
+
+  FErrors^.Push(CurlResult);
+end;
+
+function THTTPResponse.TTimeout.NameLookup : TTimeInterval;
+var
+  time : Longword = 0;
+  dtime : Double = 0;
+  CurlResult : CURLcode;
+begin
+  CurlResult := curl_easy_getinfo(FCurl, CURLINFO_NAMELOOKUP_TIME_T, @time);
+  Result := TTimeInterval.Create;
+  Result.Milliseconds := time;
+
+  if CurlResult <> CURLE_OK then
+  begin
+    CurlResult := curl_easy_getinfo(FCurl, CURLINFO_NAMELOOKUP_TIME, @dtime);
+    Result.Milliseconds := ceil(dtime);
+  end;
+
+  FErrors^.Push(CurlResult);
+end;
+
+function THTTPResponse.TTimeout.Connect : TTimeInterval;
+var
+  time : Longword = 0;
+  dtime : Double = 0;
+  CurlResult : CURLcode;
+begin
+  CurlResult := curl_easy_getinfo(FCurl, CURLINFO_CONNECT_TIME_T, @time);
+  Result := TTimeInterval.Create;
+  Result.Milliseconds := time;
+
+  if CurlResult <> CURLE_OK then
+  begin
+    CurlResult := curl_easy_getinfo(FCurl, CURLINFO_CONNECT_TIME, @dtime);
+    Result.Milliseconds := ceil(dtime);
+  end;
+
+  FErrors^.Push(CurlResult);
+end;
+
+function THTTPResponse.TTimeout.AppConnect : TTimeInterval;
+var
+  time : Longword = 0;
+  dtime : Double = 0;
+  CurlResult : CURLcode;
+begin
+  CurlResult := curl_easy_getinfo(FCurl, CURLINFO_APPCONNECT_TIME_T, @time);
+  Result := TTimeInterval.Create;
+  Result.Milliseconds := time;
+
+  if CurlResult <> CURLE_OK then
+  begin
+    CurlResult := curl_easy_getinfo(FCurl, CURLINFO_APPCONNECT_TIME, @dtime);
+    Result.Milliseconds := ceil(dtime);
+  end;
+
+  FErrors^.Push(CurlResult);
+end;
+
+function THTTPResponse.TTimeout.PreTransfer : TTimeInterval;
+var
+  time : Longword = 0;
+  dtime : Double = 0;
+  CurlResult : CURLcode;
+begin
+  CurlResult := curl_easy_getinfo(FCurl, CURLINFO_PRETRANSFER_TIME_T, @time);
+  Result := TTimeInterval.Create;
+  Result.Milliseconds := time;
+
+  if CurlResult <> CURLE_OK then
+  begin
+    CurlResult := curl_easy_getinfo(FCurl, CURLINFO_PRETRANSFER_TIME, @dtime);
+    Result.Milliseconds := ceil(dtime);
+  end;
+
+  FErrors^.Push(CurlResult);
+end;
+
+function THTTPResponse.TTimeout.StartTransfer : TTimeInterval;
+var
+  time : Longword = 0;
+  dtime : Double = 0;
+  CurlResult : CURLcode;
+begin
+  CurlResult := curl_easy_getinfo(FCurl, CURLINFO_STARTTRANSFER_TIME_T, @time);
+  Result := TTimeInterval.Create;
+  Result.Milliseconds := time;
+
+  if CurlResult <> CURLE_OK then
+  begin
+    CurlResult := curl_easy_getinfo(FCurl, CURLINFO_STARTTRANSFER_TIME, @dtime);
     Result.Milliseconds := ceil(dtime);
   end;
 
